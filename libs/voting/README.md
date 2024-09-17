e5789f6a80fe77cebb0b2a67c74709d5448dd977
Alek <slodczyk97@gmail.com>
2024-09-17 23:43:20 +0200

    added voting lib

---
+++ libs/voting/README.md
@@ -0,0 +1,7 @@
+# voting
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test voting` to execute the unit tests.
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+      path: '',
+      component: NxWelcomeComponent,
+      pathMatch: 'full',
+    },
+    {
+      path: 'voting',
+      loadComponent: () =>
+        import('@voting-app/voting').then((m) => m.VotingComponent),
+    },
+  ];
