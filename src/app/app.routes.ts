aa41ded44756203ba8b08e324a973d90d166849b
Alek <slodczyk97@gmail.com>
2024-09-20 13:45:36 +0200

    changed route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,8 @@
 import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Route[] = [
-    {
-      path: '',
-      component: NxWelcomeComponent,
-      pathMatch: 'full',
-    },
-    {
-      path: 'voting',
-      loadComponent: () =>
-        import('@voting-app/voting').then((m) => m.VotingComponent),
-    },
-  ];
+  {
+    path: '',
+    loadComponent: () => import('@voting-app/voting').then((m) => m.VotingComponent)
+  }
+];
